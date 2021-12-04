444693da95914fe184366d2120a96567522e06f5
Stasysolnze <Stasysolnze@yandex.ru>
2021-12-04 15:19:40 +0300

    Добавьте файлы проекта.

---
+++ Homework_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Дети подарили маме хризантемы";
+            string MaxLengthWord = string.Empty;
+            string[] words = s.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > MaxLengthWord.Length)
+                    MaxLengthWord = words[i];
+            }
+            Console.Write("Самое длинное слово: ");
+            Console.WriteLine(MaxLengthWord);
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Homework_6_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            string OppositeS = "";
+
+            for (int i = S.Length - 1; i >= 0; i--)
+            {
+                OppositeS += S[i];
+            }
+            if (S == OppositeS)
+                Console.WriteLine("Палиндром");
+            else
+                Console.WriteLine("Не Палиндром");
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
